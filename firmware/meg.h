c855d65136c38f146ebfb00635e8c70f63f0d092
Chalanthorn <chalanthorn.a@ku.th>
2021-04-17 14:23:36 +0700

    updated

--- firmware/meg.h
+++ firmware/meg.h
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #define CARD_CHECK1()((PINC & (1<<PC3)) == 0)
 #define CARD_CHECK2()((PINC & (1<<PC4)) == 0)
-#define REED() (~(PINB & 0b00001111))
+#define REED() (~(PORTB & 0b00001111))
 void init_peri();
 void set_led_value(uint8_t value);
 void buzzer(int value);
